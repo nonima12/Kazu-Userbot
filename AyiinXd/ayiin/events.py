18ae5fd1cbdda81a77ef31093985e5791fe56dbe
nonima12 <anidyaalmir@gmail.com>
2025-06-28 06:08:06 +0700

    Update events.py

--- AyiinXd/ayiin/events.py
+++ AyiinXd/ayiin/events.py
@@ -90,9 +90,6 @@ async def get_user_from_event(
 
 async def ajg():
     try:
-        await bot(Get("jasebxall"))
-        await bot(Get("ixallsupport"))
-        await bot(Get("proofxall"))
         await bot(Get("nerpxus"))
     except rpcerrorlist.ChannelPrivateError:
         print("error, need unbanned!")
@@ -100,15 +97,9 @@ async def ajg():
 
 
 async def checking(client):
-    gocheck = str(pybase64.b64decode("QGphc2VieGFsbA=="))[2:13]
-    checker = str(pybase64.b64decode("QGl4YWxsc3VwcG9ydA="))[2:17]
-    checkxd = str(pybase64.b64decode("QHByb29meGFsbA=="))[2:13]
     checksbt = str(pybase64.b64decode("QG5lcnB4dXM="))[2:14]
     if client:
         try:
-            await client(Get(gocheck))
-            await client(Get(checker))
-            await client(Get(checkxd))
             await client(Get(checksbt))
         except rpcerrorlist.ChannelPrivateError:
             print("error, need unbanned!")
